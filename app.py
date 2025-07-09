cf6aeec03f6ebd7baf78aff90f269f86aaa9a67a
Paula Castillo Linero <134175674+paulaisabelacastillo@users.noreply.github.com>
2025-07-08 19:05:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,19 +3,16 @@ from flask import Flask, render_template, request, redirect, url_for, session
 app = Flask(__name__)
 app.secret_key = 'clave_super_secreta'
 
-# Página principal
 @app.route("/")
 def home():
     return render_template("index.html")
 
-# Página de misiones
 @app.route("/misiones")
 def misiones():
     if "completadas" not in session:
         session["completadas"] = []
     return render_template("misiones.html", completadas=session["completadas"])
 
-# Endpoint para registrar misión completada
 @app.route("/completar/<int:dia>")
 def completar_mision(dia):
     if "completadas" not in session:
